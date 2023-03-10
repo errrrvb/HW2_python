81ff55480729d561dcc0e2c2234675408f9599cb
errrrvb <roman-vavilov123region@yandex.ru>
2023-03-10 14:44:54 +0300

    done

---
+++ 2.py
@@ -0,0 +1,8 @@
+for i in range(int(input())): # ввод количества необходимых чисел
+    N = int(input()) # ввод числа
+    if N != 0: # если введеное число != 0, то пишем NO
+        print("NO")
+    else: # иначе же пишем YES и закрываем цикл с помощью break
+        print("YES")
+        break
+
